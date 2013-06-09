d21930de3ba73c42d2248342d07048e0f570208b
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2013-06-09 21:10:23 +0200

    Changed version and restored functionalities

--- lib/generators/webfaction/install/install_generator.rb
+++ lib/generators/webfaction/install/install_generator.rb
@@ -4,19 +4,18 @@ module Webfaction
   module Generators
     class InstallGenerator < Rails::Generators::NamedBase
       
-      # Note: Not required if templates directory is in the same place
       source_root File.expand_path('../templates', __FILE__)
 
-      # desc "Creates a deploy file that is well organized and makes easy webfaction task usage"
+      desc "Creates a deploy file that is well organized and makes easy webfaction task usage"
 
-      # def copy_deploy
-      #   target_file = 'config/deploy.rb'
-      #   if File.exists?(target_file)
-      #     return unless yes?('Deploy file already exists and will be overwritten, would you like to continue?')
-      #     remove_file(target_file)
-      #   end
-      #   template "deploy.rb", target_file
-      # end
+      def copy_deploy
+        target_file = 'config/deploy.rb'
+        if File.exists?(target_file)
+          return unless yes?('Deploy file already exists and will be overwritten, would you like to continue?')
+          remove_file(target_file)
+        end
+        template "deploy.rb", target_file
+      end
 
     end
   end
