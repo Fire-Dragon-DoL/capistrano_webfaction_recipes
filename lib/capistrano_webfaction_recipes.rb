368ab4971b811a8ec8e25d92e931ac97ec5be06e
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2013-06-09 17:28:27 +0200

    Commented load_path injection

--- lib/capistrano_webfaction_recipes.rb
+++ lib/capistrano_webfaction_recipes.rb
@@ -1,3 +1,6 @@
+# lib = File.expand_path('../../lib', __FILE__)
+# $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
 require "capistrano_webfaction_recipes/version"
 
 puts "CapistranoWebfactionRecipes loaded"
