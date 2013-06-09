1be215bc4d47a23b867c0e3226bcb5692c731c1d
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2013-06-09 21:33:17 +0200

    Added some interactive functionalities

--- lib/capistrano_webfaction_recipes.rb
+++ lib/capistrano_webfaction_recipes.rb
@@ -1,2 +1,1 @@
 require "capistrano_webfaction_recipes/version"
-require "generators/webfaction/install/install_generator"
--- lib/generators/webfaction/install/install_generator.rb
+++ lib/generators/webfaction/install/install_generator.rb
@@ -1,4 +1,5 @@
 require 'rails'
+require 'active_support/all'
 
 module Webfaction
   module Generators
@@ -11,10 +12,20 @@ module Webfaction
       def copy_deploy
         target_file = 'config/deploy.rb'
         if File.exists?(target_file)
-          return unless yes?('Deploy file already exists and will be overwritten, would you like to continue?')
+          unless yes?('Deploy file already exists and will be overwritten, would you like to continue?')
+            puts 'Operation aborted'
+            return
+          end
           remove_file(target_file)
         end
-        template "deploy.rb", target_file
+        rails_app_root_dir_name        = File.basename(Dir.pwd)
+        server_ssh_user                = ask?('Insert your server ssh username')
+        server_ssh_user                = 'PUT_SERVER_SSH_USER' if server_ssh_user.blank?
+        remote_rails_app_root_dir_name = ask?('Insert your remote rails application root directory name')
+        remote_rails_app_root_dir_name = 'PUT_REMOTE_RAILS_APP_ROOT_DIR_NAME' if remote_rails_app_root_dir_name.blank?
+        bitbucket_user                 = ask?('Insert your bitbucket username')
+        bitbucket_user                 = 'PUT_BITBUCKET_USER' if bitbucket_user.blank?
+        template "deploy.rb.erb", target_file
       end
 
     end
