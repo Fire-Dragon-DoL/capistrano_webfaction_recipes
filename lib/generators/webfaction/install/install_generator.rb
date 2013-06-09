f24f13d8ae907174bfc54d4bd83ea15076ab0e91
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2013-06-09 21:12:13 +0200

    Fixed inheriting from NamedBase

--- lib/generators/webfaction/install/install_generator.rb
+++ lib/generators/webfaction/install/install_generator.rb
@@ -2,8 +2,8 @@ require 'rails'
 
 module Webfaction
   module Generators
-    class InstallGenerator < Rails::Generators::NamedBase
-      
+    class InstallGenerator < Rails::Generators::Base
+
       source_root File.expand_path('../templates', __FILE__)
 
       desc "Creates a deploy file that is well organized and makes easy webfaction task usage"
