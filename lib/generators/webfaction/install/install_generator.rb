39a576b5662ebfbdf3e67a72f9f3fc54c02cbad3
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2013-06-09 22:22:12 +0200

    Updated to remove deploy file only as last action if required

--- lib/generators/webfaction/install/install_generator.rb
+++ lib/generators/webfaction/install/install_generator.rb
@@ -10,13 +10,14 @@ module Webfaction
       desc "Creates a deploy file that is well organized and makes easy webfaction task usage"
 
       def copy_deploy
-        target_file = 'config/deploy.rb'
+        target_file                = 'config/deploy.rb'
+        old_deploy_must_be_removed = false
         if File.exists?(target_file)
           unless yes?('Deploy file already exists and will be overwritten, would you like to continue?')
             puts 'Operation aborted'
             return
           end
-          remove_file(target_file)
+          old_deploy_must_be_removed = true
         end
         @rails_app_root_dir_name        = File.basename(Dir.pwd)
         @server_ssh_user                = ask('Insert your server ssh username')
@@ -25,6 +26,8 @@ module Webfaction
         @remote_rails_app_root_dir_name = 'PUT_REMOTE_RAILS_APP_ROOT_DIR_NAME' if @remote_rails_app_root_dir_name.blank?
         @bitbucket_user                 = ask('Insert your bitbucket username')
         @bitbucket_user                 = 'PUT_BITBUCKET_USER' if @bitbucket_user.blank?
+        
+        remove_file(target_file) if old_deploy_must_be_removed
         template "deploy.rb.erb", target_file
       end
 
